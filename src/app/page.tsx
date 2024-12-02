0b17753f00f5e352fcb0ad86e1b13be2a40e6e1d
brancore87 <brancore87@gmail.com>
2024-12-02 18:34:58 +0530

    Tailwind CSS Setup & Theming

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
-    <main className="mt-5 flex flex-col items-center">
-      <h1 className="text-3xl font-bold text-violet-500">
-        Hello from Next.js 15
-      </h1>
-    </main>
+    <>
+      <h1 className="h1-bold ">Welcome to the world of Next.js 15</h1>
+    </>
   );
 }
